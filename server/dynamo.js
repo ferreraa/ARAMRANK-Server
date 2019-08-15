294d7c39de55d03e2e6e94c90aa6ac90b21cbd1e
ferreraa <alexandre78ferrera@gmail.com>
2019-08-15 13:38:25 +0200

    debugging remote table_name

--- server/dynamo.js
+++ server/dynamo.js
@@ -10,6 +10,8 @@ AWS.config.update({
 let dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 const table_name = process.env.table_name || "players_test2";
+console.log(table_name);
+
 
 function getSumByAccountId(id) {
 	var params = {
