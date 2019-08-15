4a3436227c07da4356c3d43ed61233d3300814f3
ferreraa <alexandre78ferrera@gmail.com>
2019-08-16 01:25:23 +0200

    dotenv package installed in order to manage the RIOT api key locally as well as remotely

--- server/teemo.js
+++ server/teemo.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 const champJSON = require("./champJSONManager");
 
 const TeemoJS = require('teemojs');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 let api = TeemoJS(process.env.RITO);
 
 //search Summoner data from summoner name
