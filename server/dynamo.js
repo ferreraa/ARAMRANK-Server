6f8d0628c8987d088b7322288bf76b097b8a884c
Alexandre Ferrera <alexandre78ferrera@gmail.com>
2021-04-26 01:46:12 +0200

    lint: minor linting changes

--- server/dynamo.js
+++ server/dynamo.js
@@ -93,7 +93,7 @@ function putNewSummoner(summoner) {
     var params = {
       Item: dynamoItem, 
       TableName: players_table
-     };
+    };
     dynamodb.putItem(params, function(err, data) {
        if (err) console.log(err, err.stack); // an error occurred
        else     console.log(data);           // successful response
