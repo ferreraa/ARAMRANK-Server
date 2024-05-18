d9e97ce5accf0901a01f1653e8f2c2ce0e9133bf
ferreraa <alexandre78ferrera@gmail.com>
2024-05-18 17:18:09 +0200

    new half season

--- app/src/server/dynamo.js
+++ app/src/server/dynamo.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
 const dynamodb = new AWS.DynamoDB(dynamodbConfig);
 
 
-const players_table = process.env.table_name || "Players_S14_dev";
+const players_table = process.env.table_name || "Players_S14.5_dev";
 
 const BEGINNING_OF_SEASON = process.env.beginning_of_season || 1704888000; //2024/01/10 - 12:00:00 GMT
 
